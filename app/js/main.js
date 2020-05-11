63a031530f3ee537a36815e96ee4206db21bd789
dkovalenko0 <kovalenkodima715@gmail.com>
2020-05-12 00:58:55 +0300

    create videos block

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,12 @@
+$(function(){
+	let figure = $(".video").hover( hoverVideo, hideVideo );
 
+	function hoverVideo(e) {
+	    $('video', this).get(0).play();
+	}
+
+	function hideVideo(e) {
+	    $('video', this).get(0).pause();
+	}
+
+});
